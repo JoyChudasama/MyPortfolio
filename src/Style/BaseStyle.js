9d1011ee3efa71f51cd1813636056c0d05816e5c
JoyChudasama <joychudasama11@example.com>
2020-12-05 18:41:38 -0400

    General Style

---
+++ src/Style/BaseStyle.js
@@ -0,0 +1,48 @@
+import styledComponents from 'styled-components';
+
+
+export const StyledAbout = styledComponents.div`
+    min-height : 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`
+
+export const StyledDescription = styledComponents.div`
+    flex: 1;
+    padding-right: 5rem;
+
+    h2{
+        font-weight: lighter;
+    }
+`
+
+export const StyledImage = styledComponents.div`
+    flex:0.9;
+
+    img{
+        width:90%;
+        height: 85vh;
+        object-fit:cover;
+        margin-left: 2rem;
+        border-radius:50px;
+    }
+`
+export const StyledButton = styledComponents.button`
+    font-weight: bold;
+    font-size: 1.1rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #23d997;
+    background: transparent;
+    color: white;
+    transition: all 0.5s ease;
+    
+    &:hover{
+        background-color: #23d997;
+        color:black;
+        }
+    }
+`
