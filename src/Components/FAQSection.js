b59be43206138a7aeef14e76f98e00e5db0a742c
JoyChudasama <joychudasama11@example.com>
2020-12-05 19:38:59 -0400

    Added faq-line & Styling

--- src/Components/FAQSection.js
+++ src/Components/FAQSection.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+// Style
+import {StyledFAQ} from '../Style/FAQSectionStyle';
+
 const FAQSection = () => {
     return(
-        <div className="faq">
+        <StyledFAQ>
 
             <h2>Any Question? <span>FAQ</span></h2>
             <div className="question">
@@ -11,17 +14,29 @@ const FAQSection = () => {
                     <p>Joy has Computer Programming Diploma from Lambton College at Toronto Campus; 
                         he Graduated in December 2019.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
 
             <div className="question">
                 <h4>What are Joy's Interests or Hobbies?</h4>
                 <div className="answer">
-                    <p>Well, there are bunch of things that Joy likes to do apart from Programming and it includes Sports(Cricket, Basketball, Bowling, Pool),
-                        Reading, Space Exploration, Playing FPS Games and last but not least Watching <span>FRIENDS</span> over and over again.</p>
+                    <p>Well, there are bunch of things that Joy likes to do apart from Programming and it includes Sports(Cricket, Basketball,
+                        Bowling, Pool), Reading, Space Exploration, Playing FPS Games and last but not least Watching <span>FRIENDS</span> over 
+                        and over again.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
 
-        </div>
+            <div className="question">
+                <h4>What is Joy Learning Right Now?</h4>
+                <div className="answer">
+                    <p>Currently, Joy is focusing on some his Projects but once they are done he'll back to learning Neaural Networks or
+                        Some New JS Frameworks.</p>
+                </div>
+                <div className="faq-line"></div>
+            </div>
+
+        </StyledFAQ>
 
     )
 }
