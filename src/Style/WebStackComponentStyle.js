191f50dc42f356dc690e9ff8e6e9b90b2462f111
JoyChudasama <joychudasama11@example.com>
2020-12-10 04:20:35 -0400

    Style for WebStack Component

---
+++ src/Style/WebStackComponentStyle.js
@@ -0,0 +1,32 @@
+import styledComponents from 'styled-components';
+
+export const StyledWebstackComponent = styledComponents.div`
+    padding:0 3rem;
+    h3{
+        font-size: 9rem;
+        color:#313347;
+        opacity: 60%;
+    }
+
+    .lists{
+        margin-top:3rem;
+        ul{
+            list-style: none;
+            li{ 
+                display:flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 3rem;
+                padding: 1rem 0;
+                color: #40BAD5;
+            }
+        }
+    }
+
+    .line{
+        width:30%;
+        height: 0.2rem;
+        background: #23d997;
+        margin-top: 1rem;
+    }
+`
