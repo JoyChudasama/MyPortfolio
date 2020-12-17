13ff716b7e19d9e66eccb37b5e784fd1305f6be3
JoyChudasama <joychudasama11@example.com>
2020-12-17 15:35:57 -0400

    Added Toggle Animation

--- src/Components/FAQSection.js
+++ src/Components/FAQSection.js
@@ -1,41 +1,42 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 // Style
 import {StyledFAQ} from '../Style/FAQSectionStyle';
+// Toggle Component
+import Toggle from '../ReusableComponents/Toggle'
+// Framer Motion, AnimateSharedLayout
+import {motion} from 'framer-motion';
+import {AnimateSharedLayout} from 'framer-motion';
+import {textFadeAnimation} from '../Animations/ChildElementsAnimation';
 
 const FAQSection = () => {
+    
+
     return(
         <StyledFAQ>
-
             <h2>Any Question? <span>FAQ</span></h2>
-            <div className="question">
-                <h4>What and Where Joy Studied?</h4>
-                <div className="answer">
-                    <p>Joy has Computer Programming Diploma from Lambton College at Toronto Campus; 
-                        he Graduated in December 2019.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <AnimateSharedLayout>
+                <Toggle question="What and Where Joy Studied?">
+                    <motion.div variants={textFadeAnimation} layout className="answer">
+                        <p>Joy has Computer Programming Diploma from Lambton College at Toronto Campus; 
+                            he Graduated in December 2019.</p>
+                    </motion.div>
+                </Toggle>
+                
+                <Toggle question="What are Joy's Interests or Hobbies?">
+                        <motion.div variants={textFadeAnimation} layout className="answer">
+                            <p>Well, there are bunch of things that Joy likes to do apart from Programming and it includes Sports(Cricket, Basketball,
+                                Bowling, Pool), Reading, Space Exploration, Playing FPS Games and last but not least Watching <span>FRIENDS</span> over 
+                                and over again.</p>
+                        </motion.div>
+                </Toggle>
 
-            <div className="question">
-                <h4>What are Joy's Interests or Hobbies?</h4>
-                <div className="answer">
-                    <p>Well, there are bunch of things that Joy likes to do apart from Programming and it includes Sports(Cricket, Basketball,
-                        Bowling, Pool), Reading, Space Exploration, Playing FPS Games and last but not least Watching <span>FRIENDS</span> over 
-                        and over again.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-
-            <div className="question">
-                <h4>What is Joy Learning Right Now?</h4>
-                <div className="answer">
-                    <p>Currently, Joy is focusing on some of his Projects but once they are done he'll back to learning Neaural Networks or
-                        Some New JS Frameworks.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-
+                <Toggle question="What is Joy Learning Right Now?">   
+                        <motion.div variants={textFadeAnimation} layout className="answer">
+                            <p>Currently, Joy is focusing on some of his Projects but once they are done he'll back to learning Neaural Networks or
+                                Some New JS Frameworks.</p>
+                        </motion.div>
+                </Toggle>
+            </AnimateSharedLayout>
         </StyledFAQ>
 
     )
