b52628de494faeeac65abe5dc76a33dfcaf9b792
JoyChudasama <joychudasama11@example.com>
2020-12-17 16:28:04 -0400

    Added ScrollReavel Animation

--- src/Animations/ChildElementsAnimation.js
+++ src/Animations/ChildElementsAnimation.js
@@ -39,3 +39,17 @@ export const overlaySliderAnimation = {
         transition: {ease:'easeOut', duration: 1}
     }
 }
+
+
+export const scrollReveal = {
+    hidden: {
+        opacity:0, 
+        scale: 1.2, 
+        transition:{duration:0.5}
+    },
+    show: {
+        opacity:1,
+        scale:1,
+        transition:{duration:0.5}
+    }
+}
